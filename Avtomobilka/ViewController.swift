6b7038d6d302cd87094a1363a557f484e1d44a3c
Vladimir Vaskin <ipse666@gmail.com>
2023-07-25 07:52:45 +0300

    Initial Commit

---
+++ Avtomobilka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Avtomobilka
+//
+//  Created by Vladimir Vaskin on 25.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
